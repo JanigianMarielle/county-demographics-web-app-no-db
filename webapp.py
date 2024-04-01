a54fe62b897a8ae677caccb139cc842f3bbfbf6c
JanigianMarielle <158102962+JanigianMarielle@users.noreply.github.com>
2024-04-01 14:17:02 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,7 +4,7 @@ from markupsafe import Markup
 import os
 import json
 
-app = Flask(__name__)
+app = Flask(state_fun_fact)
 
 @app.route('/')
 def home():
@@ -16,14 +16,14 @@ def home():
 def render_fact():
     states = get_state_options()
     state = request.args.get('state')
-    county = county_most_under_18(state)
-    fact = "In " + state + ", the county with the highest percentage of under 18 year olds is " + county + "."
+    county = name_orgin(state)
+    fact = "In " + state + ", The average time it takes to get to work is" + county + "."
     return render_template('home.html', state_options=states, funFact=fact)
     
 def get_state_options():
     """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
-    with open('demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
+    with open('miscellaneous.json') as miscellaneous_data:
+        time = json.load(miscellaneous_data)
     states=[]
     for c in counties:
         if c["State"] not in states:
@@ -33,18 +33,18 @@ def get_state_options():
         options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
     return options
 
-def county_most_under_18(state):
-    """Return the name of a county in the given state with the highest percent of under 18 year olds."""
-    with open('demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
+def name_origin(state):
+    """Return the way the state recieved its name."""
+    with open('miscellaneous.json') as miscellaneous:
+        time = json.load(miscellaneous_data)
     highest=0
-    county = ""
-    for c in counties:
-        if c["State"] == state:
-            if c["Age"]["Percent Under 18 Years"] > highest:
-                highest = c["Age"]["Percent Under 18 Years"]
-                county = c["County"]
-    return county
+    miscellaneous = ""
+    for t in time:
+        if t["State"] == state:
+            if t["Time"]["Number of minutes it takes to commute to work"] > highest:
+                highest = m["Age"]["Percent Under 18 Years"]
+                miscellaneous = t["Time"]
+    return time
 
 def is_localhost():
     """ Determines if app is running on localhost or not
